fa6bd439336992bd1b876e569c7f19dfacf11724
rufatbayramov90 <78435344+rufatbayramov90@users.noreply.github.com>
2021-08-30 12:51:54 +0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+function insert(num){
+    document.form.textview.value = document.form.textview.value + num ;
+}
+function equal(){
+    document.form.textview.value = eval(document.form.textview.value)
+}
